685d120f9de7a710d716c102c65dc01772767d8a
Kwounsu Lee <sb00lgs@gmail.com>
2021-03-15 16:01:14 +0900

    Create CombinationFormula.py
    Python Combination Formula "nCr" using Factorial code.

---
+++ CodeBlocks/CombinationFormula.py
@@ -0,0 +1,11 @@
+memo = [0 for _ in range(101)]
+memo[0],memo[1],after = 1,1,2
+def fact(n):
+    if memo[n] == 0:
+        for i in range(after,n+1):
+            memo[i] = memo[i-1]*i
+        after = m+1
+    return memo[n]
+
+def C(n,r):
+    return fact(n) / (fact(n-r) * fact(r))
