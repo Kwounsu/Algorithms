8c96155a27f139ee4fb3b7ea4d8054f30b5cca3a
Kwounsu Lee <sb00lgs@gmail.com>
2021-03-15 17:08:02 +0900

    Update CombinationFormula.py
    passing memoization by parameter instead of using global variable

--- CodeBlocks/CombinationFormula.py
+++ CodeBlocks/CombinationFormula.py
@@ -1,11 +1,24 @@
-memo = [0 for _ in range(101)]
-memo[0],memo[1],after = 1,1,2
-def fact(n):
+# memo = [0 for _ in range(101)]
+# memo[0],memo[1],after = 1,1,2
+# def fact(n):
+#     if memo[n] == 0:
+#         for i in range(after,n+1):
+#             memo[i] = memo[i-1]*i
+#         after = n+1
+#     return memo[n]
+
+# def C(n,r):
+#     return fact(n) / (fact(n-r) * fact(r))
+
+def fact(n,memo,after):
     if memo[n] == 0:
         for i in range(after,n+1):
             memo[i] = memo[i-1]*i
         after = n+1
     return memo[n]
 
-def C(n,r):
-    return fact(n) / (fact(n-r) * fact(r))
+def C(n,r,memo,after):
+    return int(fact(n,memo,after) / (fact(n-r,memo,after) * fact(r,memo,after)))
+
+memo = [0 for _ in range(101)]
+memo[0],memo[1],after = 1,1,2
