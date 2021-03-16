4cf6960e583ac59a4d462d5075ad15e5481018ba
Kwounsu Lee <sb00lgs@gmail.com>
2021-03-16 18:58:29 +0900

    Create Counter.py
    요즘 누가 딕셔너리로 개수 세누 카운터 쓰제

---
+++ CodeBlocks/Counter.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+Counter('hello world') # Counter({'l': 3, 'o': 2, 'h': 1, 'e': 1, ' ': 1, 'w': 1, 'r': 1, 'd': 1})
+
+Counter('hello world').most_common() # [('l', 3), ('o', 2), ('h', 1), ('e', 1), (' ', 1), ('w', 1), ('r', 1), ('d', 1)]
+
+Counter('hello world').most_common(1) # [('l', 3)]
