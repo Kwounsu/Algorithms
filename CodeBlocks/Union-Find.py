50f87dd240f1fcc1cdbf8fffdbd57e0ca51b08e2
Kwounsu Lee <sb00lgs@gmail.com>
2021-03-16 17:14:41 +0900

    Update Union-Find.py

--- CodeBlocks/Union-Find.py
+++ CodeBlocks/Union-Find.py
@@ -1,29 +1,7 @@
-def find(group, v):
-    if v == group[v]: 
-        return v
-    # path compression
-    group[v] = find(group, group[v])
-    return group[v]
-
-def union(group, rank, u, v):
-    u,v = find(group, u),find(group, v)
-    if u == v:
-        return True
-    # union by rank
-    if rank[u] < rank[v]:
-        swap(u, v)
-    group[v] = u
-    if rank[u] == rank[v]:
-        rank[u] += 1
-
-def init(group, rank, n):
-    for i in range(n):
-        group.append(i)
-        rank.append(1)
-
-def solution():
-    group, rank = [], []
-    init(group, rank, 5)
-    union(group, rank, 1,3)
-
-solution()
+def union(group,u,v):
+    if group[u] == group[v]:
+        return
+    target = group[v]
+    for i in range(len(group)):
+        if group[i] == target:
+            group[i] = group[u]
