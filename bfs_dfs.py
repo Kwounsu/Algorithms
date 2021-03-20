c6ae23f05d06ec7408756b674b17395ae6927e54
Kwounsu Lee <sb00lgs@gmail.com>
2021-03-20 12:51:30 +0900

    Create bfs_dfs.py

---
+++ bfs_dfs.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def bfs(root,graph):
+    visit = {}
+    q = deque([root])
+    
+    while q:
+        node = q.popleft()  # 큐로 먼저 넣은 것 먼저 검색하면 BFS
+        if node not in visit:
+            visit[node] = True
+            q.append(graph[word])
+            
+    return visit
+
+def dfs(root,graph):
+    visit = {}
+    stack = deque([root])
+    
+    while stack:
+        node = stack.pop()   # 스택으로 나중에 넣은 것 먼저 검색하면 DFS
+        if node not in visit:
+            visit[node] = True
+            q.append(graph[word])
+            
+    return visit
