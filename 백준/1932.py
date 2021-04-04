43cfb4f1328e166145015eacc882fa2b0cf2dbab
Kwounsu Lee <sb00lgs@gmail.com>
2021-04-04 16:31:05 +0900

    DP, 타뷸레이션, 정수 삼각형

---
+++ 백준/1932.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+triangle = [list(map(int,input().split())) for _ in range(N)]
+
+for i in range(1, N):
+    for j in range(len(triangle[i])):
+        left = triangle[i - 1][j - 1] if j - 1 >= 0 else 0
+        right = triangle[i - 1][j] if j < len(triangle[i - 1]) else 0
+        triangle[i][j] += max(left, right)
+        
+print(max(triangle[-1]))
