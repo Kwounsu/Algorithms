7acf0734ce4cc30c32d673e8a51b0365ceb42a47
Kwounsu Lee <sb00lgs@gmail.com>
2021-04-06 17:44:20 +0900

    DP, LIS(Longest Increasing Subsequence)

---
+++ 백준/11053.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+
+N = int(input())
+a = list(map(int,input().split()))
+
+dp = [0] * N
+
+for i in range(N):
+    for j in range(i):
+        if a[i] > a[j] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+    dp[i] += 1
+        
+print(max(dp))
