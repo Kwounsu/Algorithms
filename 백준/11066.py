620b0bf526a3b9adbcdbd4e0c6c7897c34d9bc63
Kwounsu Lee <sb00lgs@gmail.com>
2021-04-07 14:55:12 +0900

    DP, 최소연쇄행렬곱셈, Knuth's optimization

--- 백준/11066.py
+++ 백준/11066.py
@@ -2,7 +2,35 @@ import sys
 read = sys.stdin.readline
 MAX = sys.maxsize
 
-    
+
+# memory: 33464kb, time: 752ms
+def solution(N, A):
+    sums = [0]
+    for i in A:
+        sums.append(sums[-1] + i)
+
+    dp = [[], [0] * N, [sums[i + 2] - sums[i] for i in range(N - 1)]]
+    knuth_old = [1] * (N - 1)
+
+    for d in range(3, N + 1):
+        min_sum, knuth_new = [], []
+
+        for i in range(N - d + 1):
+            tmp_sum, tmp_knuth = MAX, knuth_old[i]
+            
+            for k in range(knuth_old[i], knuth_old[i + 1] + 2):
+                if tmp_sum >= (new_sum := dp[k][i] + dp[d - k][k + i] + sums[d + i] - sums[i]):
+                    tmp_sum, tmp_knuth = new_sum, k
+            min_sum.append(tmp_sum)
+            knuth_new.append(tmp_knuth)
+
+        dp.append(min_sum)
+        knuth_old = knuth_new
+
+    return dp[-1][-1]
+
+
+# memory: 38224kb, time: 1160ms
 def solution(N, A):
     dp = [[0] * N for _ in range(N)]
     
