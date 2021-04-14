cbdef900e4312296eee83612075f6aa8a75fe3ae
Kwounsu Lee <sb00lgs@gmail.com>
2021-04-14 14:41:33 +0900

    level 2, stack

---
+++ 프로그래머스/캐시.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+
+def solution(cache_size, cities):
+    if cache_size == 0:
+        return len(cities) * 5
+
+    exe_time = 0
+    cache = deque([])
+
+    for c in cities:
+        city = c.lower()
+
+        if city in cache:
+            cache.remove(city)
+            cache.append(city)
+            exe_time += 1
+        else:
+            if len(cache) == cache_size:
+                cache.popleft()
+            cache.append(city)
+            exe_time += 5
+
+    return exe_time
