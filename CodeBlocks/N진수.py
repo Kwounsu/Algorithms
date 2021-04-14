e6a782e9ed49a79260d2455444349c1701d86d96
Kwounsu Lee <sb00lgs@gmail.com>
2021-04-14 17:29:21 +0900

    N진수, N진법

--- CodeBlocks/N진수.py
+++ CodeBlocks/N진수.py
@@ -1,6 +1,12 @@
 def n_ary(n, base):
-    result = []
-    while n > 0:
-        n, r = divmod(n, base)
-        result.append(r)
-    return ''.join(map(str, reversed(result)))
+        T = "0123456789ABCDEF"
+        q, r = divmod(n, base)
+        return T[r] if q == 0 else n_ary(q, base) + T[r]
+
+        
+# def n_ary(n, base):
+#     result = []
+#     while n > 0:
+#         n, r = divmod(n, base)
+#         result.append(r)
+#     return ''.join(map(str, reversed(result)))
