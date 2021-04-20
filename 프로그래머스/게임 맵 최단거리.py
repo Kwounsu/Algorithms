59df15c54c58d8f150717c933f5f5509c3af3b4d
Kwounsu Lee <sb00lgs@gmail.com>
2021-04-20 14:20:00 +0900

    level 2, 길찾기, DP, bfs

--- 프로그래머스/게임 맵 최단거리.py
+++ 프로그래머스/게임 맵 최단거리.py
@@ -16,7 +16,7 @@ def solution(maps):
         for d in range(4):
             r_, c_ = r + directions[d][0], c + directions[d][1]
 
-            if 0 <= r_ < n and 0 <= c_ < m and maps[r_][c_] == 1 and dp[r_][c_] == -1:
+            if 0 <= r_ < n and 0 <= c_ < m and maps[r_][c_] != 1 and dp[r_][c_] == -1:
                 dp[r_][c_] = dp[r][c] + 1
                 q.append([r_, c_])
 
