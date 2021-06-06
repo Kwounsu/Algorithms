b3abbd16fed9f02a132e7d957aa2dfb719240165
Kwounsu Lee <sb00lgs@gmail.com>
2021-06-07 00:19:27 +0900

    행렬 곱셈 (수학, 구현)

--- 백준/2740.py
+++ 백준/2740.py
@@ -3,20 +3,19 @@ input = sys.stdin.readline
 
 # INPUT
 N, M = map(int, input().split())
-A = []
-for _ in range(N):
-    A.append(list(map(int, input().split())))
+A = [list(map(int, input().split())) for _ in range(N)]
 M, K = map(int, input().split())
-B = []
-for _ in range(M):
-    B.append(list(map(int, input().split())))
+B = [list(map(int, input().split())) for _ in range(M)]
 
-# A * B = AB
+
 def product_matrix(A, B):
     return [[sum(a*b for a, b in zip(A_row,B_col)) for B_col in zip(*B)] for A_row in A]
 
-AB = product_matrix(A, B)
 
-# Print
-for row in AB:
-    print(*row)
+def product_print(A):
+    for row in A:
+        print(*row)
+
+        
+AB = product_matrix(A, B)
+product_print(AB)
