c758376afd376732336eeb3de2d006537da49bea
Kwounsu Lee <sb00lgs@gmail.com>
2021-06-10 12:43:48 +0900

    level 4, 양방향 그래프

--- 프로그래머스/[카카오 인턴] 동굴 탐험.py
+++ 프로그래머스/[카카오 인턴] 동굴 탐험.py
@@ -2,20 +2,20 @@ from collections import deque
 
 
 def solution(n, path, order):
-    queue = deque()
-    visit, can_go = [False] * n, [False] * n
-    visit[0], can_go[0] = True, True
-    must, must2 = {}, {}
-
-    for a, b in order:
-        must[b] = a
-        must2[a] = b
-
     graph = [[] for _ in range(n)]
     for a, b in path:
         graph[a].append(b)
         graph[b].append(a)
-
+    
+    must, must2 = {}, {}
+    for a, b in order:
+        must[b] = a
+        must2[a] = b
+        
+    visit, can_go = [False] * n, [False] * n
+    visit[0], can_go[0] = True, True
+    
+    queue = deque()
     if 0 not in must:
         queue.extend(graph[0])
 
@@ -36,6 +36,4 @@ def solution(n, path, order):
                 if not visit[i]:
                     queue.append(i)
 
-    if sum(visit) == n:
-        return True
-    return False
+    return sum(visit) == n
