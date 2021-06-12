4e1493a42ff92d940d47e5df78889b25575e975d
Kwounsu Lee <sb00lgs@gmail.com>
2021-06-12 18:59:50 +0900

    숫자 카드 2(자료 구조, 이분 탐색, 해시를 사용한 집합과 맵)

--- 백준/10816.py
+++ 백준/10816.py
@@ -1,6 +1,7 @@
+# Collections.Counter 이용
 import sys
-from collections import Counter
 input = sys.stdin.readline
+from collections import Counter
 
 n = int(input())
 a_n = list(map(int, input().split()))
@@ -9,3 +10,23 @@ a_m = list(map(int, input().split()))
 
 counts = Counter(a_n)
 print(*[counts[i] for i in a_m])
+
+
+# 이진탐색 (bisect)
+import sys
+input = sys.stdin.readline
+from bisect import bisect_right, bisect_left
+
+
+def count(array, target):
+    left = bisect_left(array, target)
+    right = bisect_right(array, target)
+    return right - left
+
+
+n = int(input())
+a_n = sorted(list(map(int, input().split())))
+m = int(input())
+a_m = list(map(int, input().split()))
+
+print(*[count(a_n, i) for i in a_m])
