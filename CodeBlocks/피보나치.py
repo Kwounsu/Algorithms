14dbe60361336dcef49997f732842889d1791c04
Kwounsu Lee <sb00lgs@gmail.com>
2021-06-18 17:46:03 +0900

    fibonacci - memoization with two variables

--- CodeBlocks/피보나치.py
+++ CodeBlocks/피보나치.py
@@ -4,3 +4,11 @@ def fib(n):
     for i in range(3, n + 1):
         memo.append(memo[i - 2] + memo[i - 1])
     return memo[-1]
+
+
+# Memoization with two variables
+def fib(n):
+    first = second = 1
+    for i in range(2, n):
+        first, second = second, first + second
+    return second
