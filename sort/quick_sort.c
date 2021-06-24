5b9f98df9610d9e31afc270ae342bd9666c7a9cb
Kwounsu Lee <sb00lgs@gmail.com>
2021-06-24 21:48:07 +0900

    Update quick_sort.c

--- sort/quick_sort.c
+++ sort/quick_sort.c
@@ -65,3 +65,37 @@ int main(){
     
     return 0;
 }
+
+
+// Using Library
+#include <stdio.h>
+#include <stdlib.h>
+
+int static compare (const void* first, const void* second)
+{
+    if (*(int*)first < *(int*)second)
+        return 1;
+    else if (*(int*)first > *(int*)second)
+        return -1;
+    else
+        return 0;
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    
+    int arr[n];
+    
+    for (int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    
+    qsort(arr, n, sizeof(int), compare);
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", arr[i]);
+    }
+    
+    return 0;
+}
