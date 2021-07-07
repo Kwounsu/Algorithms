51b2e941416de4bf21ea6bcf50c55a0445495d61
Kwounsu Lee <sb00lgs@gmail.com>
2021-07-07 12:21:34 +0900

    c 배열로 스택 구현

---
+++ 정올/Intermediate_Coder/자료구조/불쾌한 날(Bad Hair Day).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int stack[80000], top = 0;
+
+int main() {
+    long long answer = 0;
+    int N, num;
+    scanf("%d", &N);
+    
+    while (N--) {
+        scanf("%d", &num);
+        while (top > 0 && stack[top - 1] < num) top--;
+        answer += top;
+        stack[top++] = num;
+    }
+    
+    printf("%lld\n", answer);
+    return 0;
+}
