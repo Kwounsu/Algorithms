deb42c5414553c9f3204c9d0b280386bf9a40d53
Kwounsu Lee <sb00lgs@gmail.com>
2021-07-07 12:27:33 +0900

    cpp vector로 스택 구현

---
+++ 정올/Intermediate_Coder/자료구조/불쾌한 날(Bad Hair Day).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    long long answer = 0;
+    int N, num;
+    cin>>N;
+    vector<int> s;
+    
+    while (N--) {
+        cin>>num;
+        while (!s.empty() && s.back() < num) s.pop_back();
+        answer += s.size();
+        s.push_back(num);
+    }
+    
+    cout<<answer;
+    return 0;
+}
