7e4c8fec69b9aefaec9b1755031e5639d458a2a0
Kwounsu Lee <sb00lgs@gmail.com>
2021-07-08 16:19:34 +0900

    재귀 탈출 recursion break out 추가

--- 정올/Intermediate_Coder/백트래킹-DFS/좋은수열.py
+++ 정올/Intermediate_Coder/백트래킹-DFS/좋은수열.py
@@ -1,22 +1,21 @@
 import sys
-
+ 
 
 def dfs(length):
     for i in range(1, length // 2 + 1):
         if ans[-2 * i:-i] == ans[-i:]: 
-            return 1
-
-    if length == n:
-        return 0
+            return
  
+    if length == n:
+        print(*ans, sep='')
+        sys.exit(0)
+  
     for i in range(1, 4):
         ans.append(i)
-        if dfs(length + 1) == 0:
-            return 0
+        dfs(length + 1)
         ans.pop()
-
-
+ 
+ 
 n = int(sys.stdin.readline())
 ans = []
 dfs(0)
-print(*ans, sep='')
