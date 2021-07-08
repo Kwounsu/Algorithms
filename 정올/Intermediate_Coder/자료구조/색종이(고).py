2579a9cdd8dd193640d722eefd57704148f389a4
Kwounsu Lee <sb00lgs@gmail.com>
2021-07-08 18:36:33 +0900

    색종이 - 3

--- 정올/Intermediate_Coder/자료구조/색종이(고).py
+++ 정올/Intermediate_Coder/자료구조/색종이(고).py
@@ -1,59 +1,38 @@
 import sys
 input = sys.stdin.readline
 
-table = [[0 for i in range(100)] for j in range(100)]
-N = int(input())
-mx = 0
-for i in range(N):
-    x, y = map(int,input().split())
-    for i in range(10):
-        for j in range(10):
-            table[x + i][y + j] = 1
-def his(h):
-    N = len(h)
-    h = [100000001] + h
-    mx = 0
-     
-    R = [N] * N
-    stk = [0]
- 
-    for i in range(1, N + 1):
-        A = h[i]
-        while h[stk[-1]] > A:
-            R[stk[-1] - 1] = i - 1
-            stk.pop()
-            if stk == []:
-                break
-        stk.append(i)
- 
-    L = [0] * N
-    stk = [N]
- 
-    for i in range(N,0,-1):
-        A = h[i]
-        while h[stk[-1]] > A:
-            L[stk[-1] - 1] = i
-            stk.pop()
-            if stk == []:
-                break
-        stk.append(i)
- 
-    for i in range(1,N + 1):
-        if mx < (R[i-1] - L[i-1]) * h[i]:
-            mx = (R[i-1] - L[i-1]) * h[i]
- 
-    return(mx)
- 
-for i in range(100):
-    h = []
-    for j in table:
-        sum = 0
-        for k in range(i,100):
-            if j[k] == 1:
-                sum += 1
-            else:
-                break
-        h.append(sum)
-    mx = max( mx, his(h))
- 
-print( mx)
+
+def find_largest_rectangle_area():
+    ans = 0
+    
+    for i in range(100):
+        for j in range(100):
+            if matrix[i][j] == 1:
+                matrix[i][j] += matrix[i - 1][j]
+
+    for r in range(1, 100):
+        for c in range(1, 100):
+            height = 100
+            for k in range(c, 100):
+                if height > 0:
+                    height = min(height, matrix[r][k])
+                    area = height * (k - c + 1)
+                    ans = max(ans, area)
+                else:
+                    break
+    return ans
+
+def attach_paper(x, y):
+    for i in range(x, x + 10):
+        for j in range(y, y + 10):
+            matrix[i][j] = 1
+
+
+n = int(input())
+matrix = [[0] * 101 for _ in range(101)]
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    attach_paper(x, y)
+
+print(find_largest_rectangle_area())
