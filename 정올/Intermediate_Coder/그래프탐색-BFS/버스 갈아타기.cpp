5651ae6541abe8170cd22ebf02288a091f25d536
Kwounsu Lee <sb00lgs@gmail.com>
2021-07-17 11:34:30 +0900

    주석 추가

--- 정올/Intermediate_Coder/그래프탐색-BFS/버스 갈아타기.cpp
+++ 정올/Intermediate_Coder/그래프탐색-BFS/버스 갈아타기.cpp
@@ -1,8 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-typedef pair<int, int> pii;
-
 struct Line {
     int x1, y1;
     int x2, y2;
@@ -17,27 +15,27 @@ int sx, sy, dx, dy;
 bool visited[8192];
 
 void bfs() {
-    queue<pii> que;
+    queue<pair<int, int>> que;
 
     // 시작점에서 탈 수 있는 버스들 큐에 넣기
     for (int i = 0; i < k; i++)
         if (lines[i].x1 <= sx && sx <= lines[i].x2 
          && lines[i].y1 <= sy && sy <= lines[i].y2) {
-            que.push(pii(1, i));
+            que.push({1, i});
             visited[i] = 1;
         }
 
     // BFS
     while (!que.empty()) {
-        pii p = que.front();
-        que.pop();
-        int u = p.second;
-        int udist = p.first;
+        pii p = que.front(); que.pop();
+        int bus_cnt = p.first;  // 버스 탄 수
+        int u = p.second;     // 
         if (lines[u].x1 <= dx && dx <= lines[u].x2 
          && lines[u].y1 <= dy && dy <= lines[u].y2) {
-            cout<<udist<<"\n";
+            cout<<bus_cnt<<"\n";
             break;
         }
+        // 탄적 없고, 환승 가능하면 버스 타기
         for (int v = 0; v < k; v++) {
             if (visited[v]) continue;
             if (lines[u].x1 <= lines[v].x2 
@@ -45,7 +43,7 @@ void bfs() {
              && lines[u].y1 <= lines[v].y2 
              && lines[v].y1 <= lines[u].y2) {
                 visited[v] = 1;
-                que.push(pii(udist+1, v));
+                que.push({bus_cnt+1, v});
             }
         }
     }
