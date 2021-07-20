d001bb218fdbb8c83577ee871c24ac402cebc20b
Kwounsu Lee <sb00lgs@gmail.com>
2021-07-20 09:52:19 +0900

    통과

--- 정올/Intermediate_Coder/탐욕알고리즘/택배.cpp
+++ 정올/Intermediate_Coder/탐욕알고리즘/택배.cpp
@@ -15,22 +15,37 @@ public:
     }
 };
 
-int n, c, m, box[2001];
+int n, c, m, box[2001], ans;
 vector<Info> info;
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
     cin>>n>>c>>m;
-    int a,b,q;
-    for (int i = 0; i < m; i++) {
+    int a,b,q,i;
+    for (i = 0; i < m; i++) {
         cin>>a>>b>>q;
         info.push_back({a,b,q});
     }
     sort(info.begin(), info.end());
 
-    for(auto i:info)
-        cout<<i.a<<' ';
+    int box_max, empty_space;
+    for(auto truck:info) {
+        // 구간 내 가장 많이 적재된 양
+        box_max = 0;
+        for (i = truck.a; i < truck.b; i++)
+            box_max = max(box_max, box[i]);
+
+        // 트럭에 추가 가능한 양
+        empty_space = min(truck.q, c - box_max);
+        ans += empty_space;
+
+        // 해당 구간 추가 적재
+        for (i = truck.a; i < truck.b; i++)
+            box[i] += empty_space;
+    }
+
+    cout<<ans;
 
     return 0;
 }
