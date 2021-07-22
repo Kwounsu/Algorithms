6e2de25c99c033cccede14d9076f0f40ed4ef890
Kwounsu Lee <sb00lgs@gmail.com>
2021-07-22 14:31:25 +0900

    Update 장비 조합.cpp

--- 정올/Pre_Advanced_Coder/Level2/장비 조합.cpp
+++ 정올/Pre_Advanced_Coder/Level2/장비 조합.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-int dp[1000001];  // i까지 봤을 때의 총 전투력이 S보다 작거나 같은 경우의 수
- 
+
+int dp[1000001];  // i까지 봤을때 가짓수
+dp[0] = 1;
+
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     int N, S, w[20], q[20];  // 슬롯수, 전투력, 중요도, 필수장착여부
@@ -10,22 +11,20 @@ int main() {
     cin>>N>>S;
     for (int i = 0; i < N; i++)
         cin>>w[i]>>q[i];
-    
-    int base = 0;
+
     for (int i = 0; i < N; i++)
-        base += w[i] * q[i];
-    
-    dp[base] = 1;
+        S -= w[i] * q[i];
+
     for (int i = 0; i < N; i++)
         for (int j = 0; j <= S; j++)
             if (j + w[i] <= S)
                 dp[j + w[i]] = (dp[j + w[i]] + dp[j]) % 1000000009;
-    
+
     int ans = 0;
     for (int i = 0; i <= S; i++)
         ans = (ans + dp[i]) % 1000000009;
-    
+
     cout<<ans;
- 
+
     return 0;
 }
