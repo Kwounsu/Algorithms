19fbabd68c6baf6cd1d753bd46be2ffded9b2667
Kwounsu Lee <sb00lgs@gmail.com>
2021-07-24 11:31:02 +0900

    Update GCD LCM.cpp

--- CodeBlocks/GCD LCM.cpp
+++ CodeBlocks/GCD LCM.cpp
@@ -1,3 +1,3 @@
 int gcd(int a, int b){return b ? gcd(b, a % b) : a;} 
 
-int lcm(int a, int b){return a * b / gcd(a, b);}
+int lcm(int a, int b){return a / gcd(a, b) * b ;}  // a * b / gcd(a,b)는 overflow의 위험성 있음.
