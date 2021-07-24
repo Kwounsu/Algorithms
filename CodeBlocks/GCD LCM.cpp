4b190718346a8e0b67e7872f87ac0501dab9eac5
Kwounsu Lee <sb00lgs@gmail.com>
2021-07-24 11:27:59 +0900

    최대공약수, 최소공배수

--- CodeBlocks/GCD LCM.cpp
+++ CodeBlocks/GCD LCM.cpp
@@ -1,3 +1,3 @@
-int gcd(int a, int b){return b? gcd(b, a%b):a;} 
+int gcd(int a, int b){return b ? gcd(b, a % b) : a;} 
 
-int lcm(int a, int b) {return a * b / gcd(a, b);}
+int lcm(int a, int b){return a * b / gcd(a, b);}
