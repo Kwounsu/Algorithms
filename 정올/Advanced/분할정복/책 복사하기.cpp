ae180147f10c6156f1d58dad4b6aea36c227aed5
Kwounsu Lee <sb00lgs@gmail.com>
2021-07-29 10:59:36 +0900

    Update 책 복사하기.cpp

--- 정올/Advanced/분할정복/책 복사하기.cpp
+++ 정올/Advanced/분할정복/책 복사하기.cpp
@@ -1,15 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print_split(int* arr, int n, int k) {
+bool split[520];
+
+void print_split(int* arr, int n) {
     int i, sum = 0;
-    for (i = n-1; i >= 0; i--) {
-        sum += arr[i];
-        if (sum > k) {
-            sum = arr[i];
+    for (int i = 0; i < n; i++) {
+        if (split[i]) {
             printf("/ ");
+            split[i] = false;
         }
         printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void check_split(int* arr, int n, int s, int k) {
+    int i, sum = 0, cnt = 1;
+    for (i = n-1; i >= 0; i--) {
+        sum += arr[i];
+        if (sum > s) {
+            sum = arr[i];
+            split[i+1] = true;
+            cnt++;
+        }
+    }
+    i = 1;
+    while (cnt < k && i < n) {
+        if (!split[i]) {
+            split[i] = true;
+            cnt++;
+        }
+        i++;
     }
 }
 
@@ -32,14 +54,15 @@ int main() {
 
     while(TC--) {
         // Input
-        int n, k, s=INT_MAX, e=0, m;
+        int n, k, s=0, e=0, m;
         scanf("%d%d", &n, &k);
         int books[n];
         for (int i = 0; i < n; i++) {
             scanf("%d", &books[i]);
-            s = min(s, books[i]);
+            s = max(s, books[i]);
             e+=books[i];
         }
+
         // binary search
         while (s <= e) {
             m = (s+e)/2;
@@ -51,7 +74,8 @@ int main() {
         }
 
         // 책 나누기 프린트
-        print_split(books, n, s);
+        check_split(books, n, s, k);
+        print_split(books, n);
     }
 
     return 0;
