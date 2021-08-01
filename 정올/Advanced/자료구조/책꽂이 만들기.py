4a88470c873f8139193f93c6c16de2930ca2ac85
Kwounsu Lee <sb00lgs@gmail.com>
2021-08-01 18:28:14 +0900

    heapq

--- 정올/Advanced/자료구조/책꽂이 만들기.py
+++ 정올/Advanced/자료구조/책꽂이 만들기.py
@@ -1,3 +1,25 @@
+import sys, heapq as hq
+input = sys.stdin.readline
+
+
+n = int(input())
+a = []
+for _ in range(n):
+    hq.heappush(a, int(input()))
+
+n -= 1
+cost = 0
+while n:
+    x = hq.heappop(a)
+    x += hq.heappop(a)
+    cost += x
+    hq.heappush(a, x)
+    n-=1
+
+print(cost)
+
+
+"""
 from queue import PriorityQueue as pq
 import sys
 input = sys.stdin.readline
@@ -15,3 +37,4 @@ while pq.qsize() > 1:
     pq.put(x)
  
 print(cost)
+"""
