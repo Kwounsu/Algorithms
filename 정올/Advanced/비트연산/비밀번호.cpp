e1b3d1d1539b3bc779e176ff1daa545d024c1ca9
Kwounsu Lee <sb00lgs@gmail.com>
2021-08-06 18:12:07 +0900

    Update 비밀번호.cpp

--- 정올/Advanced/비트연산/비밀번호.cpp
+++ 정올/Advanced/비트연산/비밀번호.cpp
@@ -1,6 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int oneCount(long long int n){
+	int cnt = 0;
+	while(n){
+        if (n % 2 != 0) cnt++;
+		n /= 2;
+	}
+	return cnt;
+}
+
+long long int bigger(int n){
+	long long int answer = n;
+	while(1){
+		answer++;
+		if(oneCount(answer) == oneCount(n)) break;
+	}
+	return answer;
+}
+
+long long int smaller(int n){
+	long long int answer = n;
+	while(1){
+		answer--;
+		if(oneCount(answer) == oneCount(n)) break;
+	}
+	return answer;
+}
+
+int main(){
+	long long int n;
+    scanf("%lld", &n);
+
+    long long int s = smaller(n);
+    printf("%lld %lld\n", s>0?s:0, bigger(n));
+}
+
+
+//=====================================================
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
 long long int bigger(long long int x){
     long long int smallest, ripple, ones;
     smallest = x&(-x);
