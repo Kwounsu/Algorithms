13c4fd25a6c2d2bedc1b6ff28c72674628b3147f
Kwounsu Lee <sb00lgs@gmail.com>
2021-08-06 20:50:38 +0900

    Fenwick Tree

--- 정올/Advanced/구간질의/동적 구간 합[1D].cpp
+++ 정올/Advanced/구간질의/동적 구간 합[1D].cpp
@@ -1,32 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 struct FenwickTree {
-    vector<int> tree;
-    FenwickTree(int n) : tree(n+1) {}
-    int sum(int pos) {
-        ++pos;
-        int ret = 0;
-        while(pos>0) {
-            ret += tree[pos];
-            pos&=(pos-1);
+    vector<ll> tree;
+    FenwickTree(ll n) : tree(n+1) {}
+    void update(ll idx, ll val) {
+        while(idx < tree.size()) {
+            tree[idx] += val;
+            idx += (idx & -idx);
+        }
+    }
+    ll sum(ll idx) {
+        ll ret = 0;
+        while(idx>0) {
+            ret += tree[idx];
+            idx -= (idx & -idx);
         }
         return ret;
-    }
-    void add(int pos, int val) {
-        ++pos;
-        while(pos < tree.size()) {
-            tree[pos] += val;
-            pos += (pos & -pos);
-        }
     }
 };
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    
-    int N;
+
+    ll N, val, M, com, idx, idx2;
     cin>>N;
+
+    vector<ll> arr(N+1);
+    FenwickTree ft = FenwickTree(N);
+
+    for (int i = 1; i <= N; i++){
+        cin>>arr[i];
+        ft.update(i, arr[i]);
+    }
+
+    cin>>M;
+    while(M--){
+        cin>>com;
+        if (com == 1){
+            cin>>idx>>val;
+            ft.update(idx, val-arr[idx]);
+            arr[idx] = val;
+        } else{
+            cin>>idx>>idx2;
+            cout<<ft.sum(idx2) - ft.sum(idx-1)<<'\n';
+        }
+    }
 
     return 0;
 }
