3d2f264316212f47c34f3bba47093be190ccaf69
Kwounsu Lee <sb00lgs@gmail.com>
2021-08-18 16:19:54 +0900

    Update 하노이2(4기둥-이동횟수만).cpp

--- 정올/Advanced/DFS/하노이2(4기둥-이동횟수만).cpp
+++ 정올/Advanced/DFS/하노이2(4기둥-이동횟수만).cpp
@@ -3,34 +3,42 @@ using namespace std;
 
 #define ulli unsigned long long int
 
-int N, n;
+int N, input[101], max_n;
 ulli h3[65], h4[1501];
 
+void hanoi3(){
+    h3[1]=1;
+    for(int i = 2; i <= 64; i++)
+        h3[i] = 2 * h3[i-1] + 1;
+}
+
 void hanoi4(int k){
-    if(h4[k-1]==0)
+    if(h4[k-1] == 0)
         hanoi4(k-1);
 
-    h4[k]=2*h4[k-1]+h3[1];
-    for(int i=1;i<64;i++){
-        if(2*(h4[k-i]-h4[k-i-1])-h3[i]-1<0)
+    h4[k] = 2 * h4[k-1] + h3[1];
+    for(int i = 1; i < 64; i++){
+        if(2 * (h4[k-i] - h4[k-i-1]) - h3[i] - 1 < 0)
             break;
-        if(2*h4[k-i]+h3[i]<h4[k])
-            h4[k]=2*h4[k-i]+h3[i];
+        if(2 * h4[k-i] + h3[i] < h4[k])
+            h4[k] = 2 * h4[k-i] + h3[i];
     }
 }
 
 int main(){
     scanf("%d",&N);
 
-    h3[1]=1;
-    for(int i=2; i<65; i++)
-        h3[i]=2*h3[i-1]+1;
-
-    h4[1]=1;
-    for(int i=0; i<N; i++){
-        scanf("%d",&n);
-        if(h4[n]==0)
-            hanoi4(n);
-        printf("%llu\n",h4[n]);
+    for(int i = 0; i < N; i++){
+        scanf("%d", &input[i]);
+        max_n = max(max_n, input[i]);
     }
+
+    hanoi3();
+    h4[1] = 1;
+    hanoi4(max_n);
+
+    for(int i = 0; i < N; i++)
+        printf("%llu\n",h4[input[i]]);
+
+    return 0;
 }
