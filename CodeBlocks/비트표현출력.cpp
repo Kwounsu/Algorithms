4bf0e7bf8db5b165fc378118329de55689e5af72
Kwounsu Lee <sb00lgs@gmail.com>
2021-08-19 10:21:54 +0900

    Update 비트표현출력.cpp

--- CodeBlocks/비트표현출력.cpp
+++ CodeBlocks/비트표현출력.cpp
@@ -4,3 +4,9 @@ void bit(int x){
         else cout<<0;
     }
 }
+
+// 함수 이름 뒤에 ll이 붙으면 long long, 아니면 int
+cout<<__builtin_clzll(n)<<'\n';       // 좌측 연속 0 개수
+cout<<__builtin_ctzll(n)<<'\n';       // 우측 연속 0 개수
+cout<<__builtin_popcountll(n)<<'\n';  // 1 개수
+cout<<__builtin_parityll(n)<<'\n';    // 1개수에 대한 패리티(짝수 or 홀수)
