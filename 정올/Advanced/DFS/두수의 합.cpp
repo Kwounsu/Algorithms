fa0b4aa17e80861856f6bd88a18c511c4d8a0ff8
Kwounsu Lee <sb00lgs@gmail.com>
2021-08-25 11:13:30 +0900

    Update 두수의 합.cpp

--- 정올/Advanced/DFS/두수의 합.cpp
+++ 정올/Advanced/DFS/두수의 합.cpp
@@ -55,3 +55,48 @@ int main() {
 
     return 0;
 }
+
+
+
+///////////////////////////////////////////////
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define endl '\n'
+int n, H, S, L, big_num, small_num;
+map<int,int> res, res_size;
+ 
+int main(){
+    cin>>n;
+    // i: small_num 자릿수. res_size에 저장됨.
+    // digit: 바뀌는 숫자(S) 자릿수.
+    for(int i=0, digit=1; digit<=n; i++, digit*=10){
+        if(n%digit%2) continue;
+        H=n/digit/11;  // 앞부분
+        S=n/digit%11;  // 중간 바뀌는 부분
+        L=n%digit/2;   // 뒷부분
+        if(S<=9 && H+S){
+            big_num=(H*10+S)*digit+L;
+            small_num = H*digit+L;
+            res[big_num]=small_num;
+            res_size[big_num]=i;
+        }
+        L=(n%digit+digit)/2;
+        S=n/digit%11-1;
+        if(S>=0 && L && H+S){
+            big_num=(H*10+S)*digit+L;
+            small_num = H*digit+L;
+            res[big_num]=small_num;
+            res_size[big_num]=i;
+        }
+    }
+ 
+    cout<<res.size()<<endl;
+    for(map<int,int>::iterator it=res.begin();it!=res.end();it++)
+        cout<<it->X<<" + "<<setw(res_size[it->X])<<setfill('0')<<it->Y<<" = "<<n<<endl;
+    return 0;
+}
