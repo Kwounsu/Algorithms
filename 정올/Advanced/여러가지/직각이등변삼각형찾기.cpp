144219607ce45422210e057f13d4e6596cb61047
Kwounsu Lee <sb00lgs@gmail.com>
2021-09-04 20:14:31 +0900

    Update 직각이등변삼각형찾기.cpp

--- 정올/Advanced/여러가지/직각이등변삼각형찾기.cpp
+++ 정올/Advanced/여러가지/직각이등변삼각형찾기.cpp
@@ -13,120 +13,116 @@ using namespace std;
      \ |  \  /  | /    /  \  / | |  \  | /  \ |
       \|   \/   |/     ---- ----  ---- |/    \|
 */
-
-int a[20][20], cnt;
-vector<pii> p;
-
-int main() {
-    ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
-
-    REP(i,1,10) REP(j,1,10) {
-        scanf("%1d", &a[i][j]);
-        cnt+=a[i][j];
-        if (cnt==1) p.push_back({i,j});
+  
+int A[15][15], T[15][15];
+  
+bool check(){
+    REP(i,0,9) REP(j,0,9)
+        if (A[i][j] != T[i][j]) return false;
+    return true;
+}
+  
+bool valid(int x, int y){
+    return (0 <= x && x < 10 && 0 <= y && y < 10);
+}
+  
+bool f1(int x, int y, int k){
+    REP(i,0,k-1) REP(j,0,i){
+        int nx = i + x, ny = j + y;
+        if (!valid(nx, ny)) return false;
+        T[nx][ny] = 1;
     }
-    debug(p[0].X);
-    debug(p[0].Y);
-
-    bool flag = true;
-    int x = p[0].X, y = p[0].Y, side;
-    if (a[x][y+1]){
-        while(a[x][y+1]) y++;
-        p.push_back({x,y});
-        side = p[1].Y - p[0].Y;
-        debug(x+side/2);
-        debug(p[0].Y+side/2);
-        if (a[x+side][p[0].Y]){
-            p.push_back({x+side,p[0].Y});
-            REP(i, p[0].X, p[0].X+side)
-                REP(j, p[0].Y, p[0].Y+side-i+p[0].X) {
-                    if (a[i][j]) cnt--;
-                    else flag = false;
-                    a[i][j] = 2;
-                }
-        }else if (a[x+side][p[1].Y]){
-            p.push_back({x+side,p[1].Y});
-            REP(i, p[0].X, p[0].X+side)
-                for(int j=p[0].Y+side; j>=p[0].Y+i-p[0].X; j--) {
-                    if (a[i][j]) cnt--;
-                    else flag = false;
-                    a[i][j] = 3;
-                }
-        }else if (a[x+side/2][p[0].Y+side/2]){
-            p.push_back({x+side,p[0].Y+side/2});
-            int st = p[0].Y, ed = p[1].Y;
-            REP(i, p[0].X, p[0].X+side)
-                REP(j, st, ed) {
-                    if (a[i][j]) cnt--;
-                    else flag = false;
-                    st++; ed--;
-                    a[i][j] = 4;
-                }
+    return true;
+}
+  
+bool f2(int x, int y, int k){
+    REP(i,0,k-1) REP(j,0,k-i-1){
+        int nx = i + x, ny = j + y;
+        if (!valid(nx, ny)) return false;
+        T[nx][ny] = 1;
+    }
+    return true;
+}
+  
+bool f3(int x, int y, int k){
+    REP(i,0,k-1)
+        for (int j=0; j>i-k; j--){
+            int nx = i + x, ny = j + y;
+            if (!valid(nx, ny)) return false;
+            T[nx][ny] = 1;
         }
-    }else{
-        while(a[x+1][y]) x++;
-        side = p[1].X - p[0].X;
-        if (a[p[1].X][p[1].Y-1] && a[p[1].X][p[1].Y+1]){
-            p.push_back({x,y+side});
-            p.push_back({x,y-side});
-            int st = p[0].Y, ed = p[0].Y;
-            REP(i, p[0].X, p[0].X+side)
-                REP(j, st, ed) {
-                    if (a[i][j]) cnt--;
-                    else flag = false;
-                    st--; ed++;
-                    a[i][j] = 5;
-                }
-        } else if (a[p[1].X][p[1].Y-1]){
-            p.push_back({x,y});
-            p.push_back({x,y-side});
-            REP(i, p[0].X, p[0].X+side)
-                for(int j=y; j>=y-i+p[0].X; j--) {
-                    if (a[i][j]) cnt--;
-                    else flag = false;
-                    a[i][j] = 6;
-                }
-        } else if (a[p[1].X][p[1].Y+1]){
-            p.push_back({x,y});
-            p.push_back({x,y+side});
-            REP(i, p[0].X, p[0].X+side)
-                REP(j, p[0].Y, p[0].Y+side+i-p[0].X) {
-                    if (a[i][j]) cnt--;
-                    else flag = false;
-                    a[i][j] = 7;
-                }
-        } else if(a[p[0].X+side/2][p[0].Y+side/2]){
-            p.push_back({x,y});
-            p.push_back({p[0].X+side/2,p[0].Y+side/2});
-            REP(i, p[0].X, p[1].X) REP(j, y, y+i-p[0].X){
-                if (a[i][j]) cnt--;
-                else flag = false;
-            }
-            REP(i, p[1].X+1, x) REP(j, y, y-i+p[1].X+1){
-                if (a[i][j]) cnt--;
-                else flag = false;
-            }
-        } else if(a[p[0].X+side/2][p[0].Y-side/2]){
-            p.push_back({x,y});
-            p.push_back({p[0].X+side/2,p[0].Y-side/2});
-            REP(i, p[0].X, p[1].X)
-                for(int j=y; j>=y; j--){
-                    if (a[i][j]) cnt--;
-                    else flag = false;
-                }
-            REP(i, p[1].X+1, x)
-                for(int j=y; j>=y; j--){
-                    if (a[i][j]) cnt--;
-                    else flag = false;
-                }
+    return true;
+}
+  
+bool f4(int x, int y, int k){
+    REP(i,0,k-1){
+        for (int j = 0; j > -i - 1; j--){
+            int nx = i + x, ny = j + y;
+            if (!valid(nx, ny)) 
+                return false;
+            T[nx][ny] = 1;
         }
     }
-
-    if (flag && cnt==0){
-        sort(p.begin(), p.end());
-        REP(i,0,2) cout<<p[i].X<<' '<<p[i].Y<<endl;
-    } else cout<<0<<endl;
-
-    //REP(i,1,10) {REP(j,1,10) cout<<a[i][j]; cout<<endl;}
-    return 0;
+    return true;
+}
+  
+bool f5(int x, int y, int k){
+    return (k % 2 == 1) && f1(x - k / 2, y, k / 2 + 1) && f2(x, y, k / 2 + 1);
+}
+  
+bool f6(int x, int y, int k){
+    return (k % 2 == 1) && f2(x, y, k / 2 + 1) && f3(x, y, k / 2 + 1);
+}
+  
+bool f7(int x, int y, int k){
+    return (k % 2 == 1) && f4(x - k / 2, y, k / 2) && f3(x, y, k / 2 + 1);
+}
+ 
+bool f8(int x, int y, int k){
+    return (k % 2 == 1) && f1(x, y, k / 2 + 1) && f4(x, y, k / 2 + 1);
+}
+  
+bool f(int p, int x, int y, int k){
+    if (p == 1) return f1(x, y, k);
+    if (p == 2) return f2(x, y, k);
+    if (p == 3) return f3(x, y, k);
+    if (p == 4) return f4(x, y, k);
+    if (p == 5) return f5(x, y, k);
+    if (p == 6) return f6(x, y, k);
+    if (p == 7) return f7(x, y, k);
+    if (p == 8) return f8(x, y, k);
+    return false;
+}
+  
+vector<pair<int, int>> point(int p, int x, int y, int k)
+{
+    vector<pair<int, int>> ans;
+    if (p == 1) ans = { {x, y}, {x + k - 1, y}, {x + k - 1, y + k - 1} };
+    if (p == 2) ans = { {x, y}, {x, y + k - 1}, {x + k - 1, y} };
+    if (p == 3) ans = { {x, y}, { x + k - 1, y }, { x, y - k + 1 } };
+    if (p == 4) ans = { {x, y}, {x + k - 1, y}, {x + k - 1, y - k + 1} };
+    if (p == 5) ans = { {x - k / 2, y}, {x + k / 2, y}, {x, y + k / 2} };
+    if (p == 6) ans = { {x, y + k / 2}, {x, y - k / 2}, {x + k / 2, y} };
+    if (p == 7) ans = { {x - k / 2, y}, {x + k / 2, y}, {x, y - k / 2} };
+    if (p == 8) ans = { {x, y}, {x + k / 2, y + k / 2}, {x + k / 2, y - k / 2} };
+    sort(ans.begin(), ans.end());
+    return ans;
+}
+  
+signed main(){
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+     
+    REP(i,0,9) REP(j,0,9)
+        scanf("%1d", &A[i][j]);
+     
+    REP(p,1,8) REP(i,0,9) REP(j,0,9) REP(k,2,10){
+        memset(T, 0, sizeof(T));
+        if (f(p, i, j, k) && check()){
+            for (auto i : point(p, i + 1, j + 1, k))
+                cout<<i.first <<' '<<i.second<<endl;
+            return 0;
+        }
+    }
+     
+    cout << 0;
 }
