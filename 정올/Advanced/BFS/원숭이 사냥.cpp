0695b38b2e7270784c0a8f2a4ccb61cf77c059e9
Kwounsu Lee <sb00lgs@gmail.com>
2021-09-07 21:22:26 +0900

    Update 원숭이 사냥.cpp

--- 정올/Advanced/BFS/원숭이 사냥.cpp
+++ 정올/Advanced/BFS/원숭이 사냥.cpp
@@ -1,75 +1,74 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define pii pair<int,int>
-#define X first
-#define Y second
 
-// N(1≤N≤21)과 이웃한 나무의 쌍 M(0≤M≤210)
-int n, m, cur, tmp, nxt, step;
-vector<vector<int>> adj;
-vector<bool> visited;
-queue<pii> q;
-pii path[1<<21];
-stack<int> ans;
+struct data{int bit, step;};
+vector<int> adj[30];
+queue<data> q;
+int visited[1<<21], path[1<<21], pr[1<<21], n,m;
 
-void track(int x){
-    if (path[x].X == 0) return;
-    track(path[x].X);
-    cout<<" "<<path[x].Y;
+void init(){
+    for(int i=0; i<30; i++) adj[i].clear();
+    while(!q.empty()) q.pop();
+    for(int i=0; i<(1<<21); i++) 
+        visited[i] = path[i] = pr[i] = 0;
 }
-
+ 
+void print(int n, int a){
+    if(a == (1<<n)-1) return;
+    print(n, path[a]);
+    printf("%d ", pr[a]);
+}
+ 
 void bfs(){
-    q.push({(1<<n)-1,1});
+    q.push({(1<<n)-1, 1});
+    visited[(1<<n)-1] = 1;
+    pr[(1<<n)-1] = -1;
     while(!q.empty()){
-        cur = q.front().X;
-        step = q.front().Y;
+        int cur = q.front().bit, step = q.front().step;
         q.pop();
-        if(visited[cur]) continue;
-        visited[cur] = true;
-        if(__builtin_popcount(cur)==1) {
-            cout<<step<<":";
-            track(0);
-            cout<<'\n';
-            return;
-        }
-        // 총쏘기
-        for(int i=0;i<n;i++){
-            if(!(cur^(1<<i))) continue;
-            tmp = cur & ~(1<<i);
-            nxt = 0;
-            // 비트마스크로 경우의수 따져보기
-            for(int j=n-1;j>=0;j--)
-                for(int a:adj[j])
-                    if((tmp>>(n-1-a)) & 1) { // a번째 비트가 1이면
-                        nxt |= (1<<j);       // j번째 비트 1로 세팅
+        if(cur == 0) break;
+        for(int i=0; i<n; i++){
+            int tmp = cur^(1<<i);
+            int temp2 = 0;
+            for(int j=0; j<n; j++){
+                int sz = adj[j].size();
+                bool flag = false;
+                for(int k=0; k<sz; k++){
+                    if((tmp>>adj[j][k])%2){
+                        flag = true;
                         break;
                     }
-            path[nxt]={cur,i};
-            q.push({nxt,step+1});
+                }
+                temp2 += flag<<j;
+            }
+            if(visited[temp2] == 0){
+                q.push({temp2, step+1});
+                path[temp2] = cur;
+                visited[temp2] = step+1;
+                pr[temp2] = i;
+            }
         }
     }
-    cout<<"Impossible\n";
+    if(visited[0] == 0) printf("Impossible\n");
+    else{
+        printf("%d: ", visited[0]-1);
+        print(n, 0);
+        printf("\n");
+    }
 }
-
+ 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
-
     while(1){
-        cin>>n>>m;
-        if (n==0 && m==0) break;
-
-        adj.clear(); adj.resize(1<<n);
-        visited.resize(1<<n, false);
-        int a,b;
-        while(m--){
-            cin>>a>>b;
+        init();
+        scanf("%d %d", &n, &m);
+        if(n==0 && m==0) break;
+        for(int i=0; i<m; i++){
+            int a, b;
+            scanf("%d %d", &a, &b);
             adj[a].push_back(b);
             adj[b].push_back(a);
         }
         bfs();
-        cout<<'\n';
     }
-
     return 0;
 }
