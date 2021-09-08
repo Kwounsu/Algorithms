bd5fab33f15159a2f3a5cb735dd6f8a1713053bc
Kwounsu Lee <sb00lgs@gmail.com>
2021-09-08 10:56:02 +0900

    bfs, bitmask

--- 정올/Advanced/BFS/원숭이 사냥.cpp
+++ 정올/Advanced/BFS/원숭이 사냥.cpp
@@ -1,70 +1,72 @@
 #include<bits/stdc++.h>
+#define X first
+#define Y second
 using namespace std;
 
-struct data{int bit, step;};
-vector<int> adj[30];
-queue<data> q;
-int visited[1<<21], path[1<<21], pr[1<<21], n,m;
+int n, m, step, cur, tmp, nxt;
+vector<int> adj[22];
+queue<pair<int,int>> q;  // <bit, step>
+int visited[1<<21], path[1<<21], path_i[1<<21];
 
-void init(){
-    for(int i=0; i<30; i++) adj[i].clear();
-    while(!q.empty()) q.pop();
-    for(int i=0; i<(1<<21); i++) 
-        visited[i] = path[i] = pr[i] = 0;
+void track(int x){
+    if(x == (1<<n)-1) return;
+    track(path[x]);
+    cout<<' '<<path_i[x];
 }
- 
-void print(int n, int a){
-    if(a == (1<<n)-1) return;
-    print(n, path[a]);
-    printf("%d ", pr[a]);
-}
- 
+
 void bfs(){
     q.push({(1<<n)-1, 1});
     visited[(1<<n)-1] = 1;
-    pr[(1<<n)-1] = -1;
+    path_i[(1<<n)-1] = -1;
     while(!q.empty()){
-        int cur = q.front().bit, step = q.front().step;
-        q.pop();
+        cur = q.front().X, step = q.front().Y; q.pop();
         if(cur == 0) break;
         for(int i=0; i<n; i++){
-            int tmp = cur^(1<<i);
-            int temp2 = 0;
+            tmp = cur^(1<<i);  // 총쏘기
+            nxt = 0;
             for(int j=0; j<n; j++){
                 int sz = adj[j].size();
-                bool flag = false;
                 for(int k=0; k<sz; k++){
-                    if((tmp>>adj[j][k])%2){
-                        flag = true;
+                    if((tmp>>adj[j][k]) & 1){
+                        nxt |= (1<<j);
                         break;
                     }
                 }
-                temp2 += flag<<j;
             }
-            if(visited[temp2] == 0){
-                q.push({temp2, step+1});
-                path[temp2] = cur;
-                visited[temp2] = step+1;
-                pr[temp2] = i;
+            if(!visited[nxt]){
+                q.push({nxt, step+1});
+                path[nxt] = cur;
+                visited[nxt] = step+1;
+                path_i[nxt] = i;
             }
         }
     }
-    if(visited[0] == 0) printf("Impossible\n");
+    if(!visited[0]) cout<<"Impossible\n";
     else{
-        printf("%d: ", visited[0]-1);
-        print(n, 0);
-        printf("\n");
+        cout<<visited[0]-1<<':';
+        track(0);
+        cout<<'\n';
     }
 }
- 
+
+void init(){
+    for(int i=0; i<22; i++) adj[i].clear();
+    while(!q.empty()) q.pop();
+    memset(visited,0,sizeof(visited));
+    memset(path   ,0,sizeof(path));
+    memset(path_i ,0,sizeof(path_i));
+}
+
 int main(){
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
     while(1){
         init();
-        scanf("%d %d", &n, &m);
+        cin>>n>>m;
         if(n==0 && m==0) break;
+        int a, b;
         for(int i=0; i<m; i++){
-            int a, b;
-            scanf("%d %d", &a, &b);
+            cin>>a>>b;
             adj[a].push_back(b);
             adj[b].push_back(a);
         }
