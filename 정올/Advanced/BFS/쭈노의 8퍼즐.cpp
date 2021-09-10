82a4bff6b1d8545dd7821ced120ede4dce58c359
Kwounsu Lee <sb00lgs@gmail.com>
2021-09-10 17:00:50 +0900

    Create 쭈노의 8퍼즐.cpp

---
+++ 정올/Advanced/BFS/쭈노의 8퍼즐.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> adj{{},
+    {2,4},{1,3,5},{2,6},
+    {1,5,7},{2,4,6,8},{3,5,9},
+    {4,8},{5,7,9},{6,8}};
+unordered_map<int,int> visited;
+struct dat{int arr[10],step;};
+queue<dat> q;
+
+
+int num(int a[]){
+    int ret = 0;
+    for(int i=1;i<10;i++)
+        ret = ret*10 + a[i];
+    return ret;
+}
+
+int loc(int a[]){
+    for(int i=1;i<10;i++)
+        if(a[i]!=i) return i; 
+    return -1;
+}
+
+void bfs(){
+    while(!q.empty()){
+        dat cur=q.front(); q.pop();
+        int n = num(cur.arr);
+        if(n == 123456780){
+            cout<<cur.step<<'\n';
+            trace(cur.)
+            break;
+        }
+        if(visited[n]) continue;
+        visited[n] = 1;
+        int x = loc(cur.arr);
+        for(int el:adj[x]){
+            int narr[10] = cur.arr;
+            narr[x] = narr[el];
+            narr[el] = 0;
+            q.push({narr,cur.step+1,cur.arr[el]});
+        }
+    }
+}
+
+int main(){
+    cin.tie(0)->sync_with_stdio(false);
+    int arr[10];
+    char ch;
+//    for(int i=1;i<10;i++){
+//        cin>>ch;
+//        arr[i] = ch!='X'?ch-'0':0;
+//    }
+    ifstream fin("input.txt");
+    for(int i=1;i<10;i++){
+        fin>>ch;
+        arr[i] = ch!='X'?ch-'0':0;
+    }
+
+    q.push({arr,0});
+    bfs();
+
+//    for(int i=1;i<10;i++){
+//        cout<<i<<": ";
+//        for(auto x:adj[i]){
+//            cout<<x<<' ';
+//        }
+//        cout<<'\n';
+//    }
+
+//    for(int i=1;i<10;i++)
+//        cout<<arr[i]<<' ';
+//    cout<<'\n';
+
+    return 0;
+}
